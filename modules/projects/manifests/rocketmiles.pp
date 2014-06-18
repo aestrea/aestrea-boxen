eb1d5e1e504384972fa26982f58f0b53bf574185
Bryan Chug <bryan.chug@gmail.com>
2014-06-19 02:02:30 +0800

    Added rocketmiles-email and rocketmiles-web

--- modules/projects/manifests/rocketmiles.pp
+++ modules/projects/manifests/rocketmiles.pp
@@ -1,6 +1,16 @@
 class projects::rocketmiles {
-	boxen::project{ 'rocketmiles':
+
+  boxen::project{ 'rocketmiles':
 		source => 'khelenek/noonu-app',
 		mysql => 'noonu'
 	}
+
+  boxen::project{ 'rocketmiles-email':
+		source => 'rocketmiles/rocketmiles-email'
+	}
+
+	boxen::project{ 'rocketmiles-web':
+		source => 'rocketmiles/rocketmiles-web'
+	}
+
 }
