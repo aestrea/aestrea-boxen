9800852daaae19bf608b3fb85eb9b8227266a803
Bryan Chug <bryan.chug@gmail.com>
2017-08-15 04:46:24 +0800

    Update

--- modules/people/manifests/aestreanova.pp
+++ modules/people/manifests/aestreanova.pp
@@ -1,5 +1,11 @@
-class people::aestreanova {
+class people::aestreanoob {
 
   include projects::rocketmiles
-  
+  include projects::aestrea_osx
+  include projects::rocketmiles
+  include projects::hotelstorm
+
+  include atom
+
+
 }
